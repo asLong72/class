617afd8cd332eac1c4b0194c4d813504f82a84c8
珏龙 陈 <1494854781@qq.com>
2019-11-28 22:11:49 +0800

    添加项目文件。

---
+++ 类.py
@@ -0,0 +1,7 @@
+
+class Dog():
+    """             """
+    def __init__(self, a ,b , c):           #用于初始化这个类的实例(self)的属性self.xxx
+        self.something1 = a
+        self.something2 = b
+        self.something3 = c
