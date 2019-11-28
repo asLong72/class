02af50f9aa0a29c0f97e082a5577fa82d571d302
珏龙 陈 <1494854781@qq.com>
2019-11-28 22:16:49 +0800

    创建于11/28/22:16

--- 类.py
+++ 类.py
@@ -1,7 +1,12 @@
 
-class Dog():
+#创建一个类
+class Dog():                                
     """             """
     def __init__(self, a ,b , c):           #用于初始化这个类的实例(self)的属性self.xxx
+                                            #需要传入的实参不包括self
         self.something1 = a
         self.something2 = b
         self.something3 = c
+
+#使用这个类
+my_dog=Dog('doge','3','male')               #为类的self的变量传入实参, 形成一个这个类的实例my_dog
